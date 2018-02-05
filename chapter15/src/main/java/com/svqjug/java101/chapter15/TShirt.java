ecae50669ef578ce427b86a119e00bac17664bcf
frankamente <fran29400@gmail.com>
2018-02-05 21:19:08 +0100

    Completed All tests.

--- chapter15/src/main/java/com/svqjug/java101/chapter15/TShirt.java
+++ chapter15/src/main/java/com/svqjug/java101/chapter15/TShirt.java
@@ -1,5 +1,27 @@
 package com.svqjug.java101.chapter15;
 
+import java.util.Objects;
+
 public class TShirt {
 
+    private final String description;
+
+    public TShirt(String description) {
+
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TShirt tShirt = (TShirt) o;
+        return Objects.equals(description, tShirt.description);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(description);
+    }
 }
--- chapter15/src/main/java/com/svqjug/java101/chapter15/TShirtsDrawer.java
+++ chapter15/src/main/java/com/svqjug/java101/chapter15/TShirtsDrawer.java
@@ -1,5 +1,33 @@
 package com.svqjug.java101.chapter15;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class TShirtsDrawer {
 
+    private List<TShirt> tShirtsCollection;
+
+    public TShirtsDrawer(){
+        tShirtsCollection = new ArrayList<>();
+    }
+    public int size() {
+        return tShirtsCollection.size();
+    }
+
+    public void add(TShirt tshirt) {
+        if(tshirt== null){
+            return;
+        }
+        tShirtsCollection.add(tshirt);
+    }
+
+    public Optional<TShirt> remove() {
+        if (tShirtsCollection.isEmpty()){
+            return Optional.empty();
+        }
+        TShirt removed = tShirtsCollection.get(0);
+        tShirtsCollection.remove(removed);
+        return Optional.of(removed);
+    }
 }
