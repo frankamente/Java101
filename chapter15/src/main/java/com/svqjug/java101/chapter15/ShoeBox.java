02061a10e7b1c6f083396baeccfd86b8c1994880
frankamente <fran29400@gmail.com>
2018-02-05 19:46:40 +0100

    ShoeBoxesTest completed.

--- chapter15/src/main/java/com/svqjug/java101/chapter15/ShoeBox.java
+++ chapter15/src/main/java/com/svqjug/java101/chapter15/ShoeBox.java
@@ -1,5 +1,27 @@
 package com.svqjug.java101.chapter15;
 
+import java.util.Objects;
+
 public class ShoeBox {
 
+    private final String name;
+
+    public ShoeBox(String name) {
+
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ShoeBox shoeBox = (ShoeBox) o;
+        return Objects.equals(name, shoeBox.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
+    }
 }
--- chapter15/src/main/java/com/svqjug/java101/chapter15/ShoeBoxes.java
+++ chapter15/src/main/java/com/svqjug/java101/chapter15/ShoeBoxes.java
@@ -1,5 +1,37 @@
 package com.svqjug.java101.chapter15;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ShoeBoxes {
 
+    Set<ShoeBox> shoeBoxCollection;
+
+    public ShoeBoxes(){
+        shoeBoxCollection = new HashSet<>();
+    }
+
+    public int size() {
+        return shoeBoxCollection.size();
+    }
+
+    public void add(ShoeBox shoeBox) {
+        if(shoeBox== null){
+            return;
+        }
+        shoeBoxCollection.add(shoeBox);
+    }
+
+    public boolean contains(ShoeBox shoeBox) {
+        return shoeBoxCollection.contains(shoeBox);
+    }
+
+    public void remove(ShoeBox shoeBox) {
+        if (shoeBox== null){
+            return;
+        }
+        shoeBoxCollection.remove(shoeBox);
+    }
 }
